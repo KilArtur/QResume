12025207148942becc0660082f93ee25dfd9caf5
KilArtur <kil_artur@inbox.ru>
2025-05-01 16:34:41 +0300

    upload files in skills

--- server/src/endpoints/api/health.py
+++ server/src/endpoints/api/health.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter
+from datetime import datetime
+from utils.loger import get_logger
 
 router = APIRouter()
+log = get_logger("health_endpoint")
 
 @router.get("/health")
 async def health():
-    return {"status": "It's OK, nice work"}
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    log.info(f"Health check выполнен в {current_time}")
+    return {"status": "It's OK, nice work", "time": current_time}
--- server/src/endpoints/api/skills.py
+++ server/src/endpoints/api/skills.py
@@ -1,14 +1,34 @@
-import json
+import os
+import tempfile
+from typing import Dict, Any
 
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, File, HTTPException
+from utils.loger import get_logger
 
 from services.OCRService import OCRService
+from endpoints.models.recognize_response import CandidateProfile
 
 router = APIRouter()
+log = get_logger("skills_endpoint")
 
-@router.get("/skills")
-async def hello():
-    ocr_service = OCRService()
-    file_path = './data/Kildiyarov.pdf'
-    result, input_token, output_token = await ocr_service.process_file(file_path)
-    return result
+@router.post("/skills", response_model=CandidateProfile)
+async def process_resume(file: UploadFile = File(...)):
+    try:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(file.filename)[1]) as temp_file:
+            temp_file_path = temp_file.name
+            content = await file.read()
+            temp_file.write(content)
+        
+        log.info(f"Файл {file.filename} успешно загружен и сохранен во временный файл")
+        
+        ocr_service = OCRService()
+        result, input_token, output_token = await ocr_service.process_file(temp_file_path)
+        
+        log.info(f"Файл успешно обработан. Использовано токенов: вход={input_token}, выход={output_token}")
+        
+        os.unlink(temp_file_path)
+        
+        return result
+    except Exception as e:
+        log.error(f"Ошибка при обработке файла: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Ошибка при обработке файла: {str(e)}")
